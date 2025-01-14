6806f73ae6a31da1f4c5f8b4ac4901a42d6a3504
Sandstormer <ert__@hotmail.com>
2025-01-13 22:38:00 -0500

    Mobile display support.

--- script.js
+++ script.js
@@ -6,19 +6,44 @@
 //   {categ:'Move', value:moveIDnum},
 // ];
 
+// do virtual scrolling
+// do fid and fidtoname
+// do big and variant sprites
+// add egg moves
+// fix move display source
+
 const itemList = document.getElementById('itemList');
 const searchBox = document.getElementById('searchBox');
 searchBox.focus();
-const headerRow = document.querySelector('.header-row');
+// const headerContainer = document.querySelector('.header-container');
+const headerContainer = document.getElementById("header-container");
 const filterContainer = document.getElementById("filter-container");
 const suggestions = document.getElementById("suggestions");
+let renderLimit = 0; // Start with no items
+const increment = 50; // Number of items to load at a time
+let showMoveLearn = false // Whether to show the sources of a filtered move
 let suggestionPreview = null;
 let lockedFilters = [];
+let isMobile = false;
+let filteredItems = null;
 
-// Track the current sort state
-let sortState = { column: 'dexno', ascending: true };
-// Track current sorted column
-let currentTarget = document.querySelector('.header-row span');
+// Set up the header columns
+let headerColumns = [];
+const headerNames = ['Dex', '', 'Species', 'Types', 'Abilities', 'BST', 'HP', 'Atk', 'Def', 'SpA', 'SpD', 'Spe'];
+const sortAttributes = ['rowno', null, 'spec', 'type1', 'ab1', 'bst', 'hp', 'atk', 'def', 'spa', 'spd', 'spe'];
+headerNames.forEach((thisHeaderName, index) => {
+  const newColumn = document.createElement('div');
+  if (thisHeaderName == '') {const img = document.createElement('img'); img.src = 'images/blank.png'; newColumn.appendChild(img);
+                     } else {newColumn.innerHTML = thisHeaderName; }
+  newColumn.sortattr = sortAttributes[index];
+  newColumn.textDef = thisHeaderName;
+  newColumn.className = 'header-column';
+  newColumn.width = 40;
+  newColumn.addEventListener('click', () => {updateHeader(newColumn)} );
+  headerColumns.push(newColumn); // Push the column element into the array
+});
+let sortState = { column: null, ascending: true }; // Track the current sort state
+let currentTarget = null; // Track current sorted column
 
 let dictOfSets = { // TEST FOR PREFILTERING
   groupA: new Set([1, 2, 3]),
@@ -27,10 +52,10 @@ let dictOfSets = { // TEST FOR PREFILTERING
 };
 
 // Display items based on query and locked filters
-function displayItems() {
+function refreshAllItems() {
   const query = searchBox.value.toLowerCase();
   itemList.innerHTML = ""; // Clear existing items
-  let filteredItems = items
+  filteredItems = items;
   // if (suggestionPreview != null) {
   //   filteredItems = items.filter(item =>
   //     // item.spec.toLowerCase().includes(suggestionPreview.value.toLowerCase())
@@ -44,14 +69,18 @@ function displayItems() {
       item.hab.toLowerCase().includes(query) ||
       item.pas.toLowerCase().includes(query) ||
       item.type1.toLowerCase().includes(query) ||
-      item.type2.toLowerCase().includes(query)
-      || suggestionPreview?.value.trim().toLowerCase().replace(/\s+/g, '') in item 
+      item.type2.toLowerCase().includes(query) ||
+      item.dexno.toString().includes(query)
+      // || suggestionPreview?.value.trim().toLowerCase().replace(/\s+/g, '') in item 
+      // || suggestionPreview.some((p) => p.value in item)
   );}
   // if (query.length === 1) { // Prefilter
   //     let uu = 'groupA';
   //     filteredItems = items.filter((_, index) => dictOfSets[uu].has(index))
   //     console.log(query.length)
   // }
+  showMoveLearn = false
+  let moveToLearn = null
   if (lockedFilters.length > 0) {
     lockedFilters.forEach(thisLockedFilter => { // Apply the locked filters
       if (thisLockedFilter.categ == "Type") { // Search for types in either slot
@@ -62,8 +91,9 @@ function displayItems() {
         || item.hab.includes(thisLockedFilter.value) || item.pas.includes(thisLockedFilter.value))
       }
       if (thisLockedFilter.categ == "Move") { // Search for moves with their designated dict key
-        filteredItems = filteredItems.filter(item => 
-          thisLockedFilter.value.trim().toLowerCase().replace(/\s+/g, '') in item )
+        let moveName = thisLockedFilter.value.trim().toLowerCase().replace(/\s+/g, '');
+        filteredItems = filteredItems.filter(item => moveName in item )
+        // if (!showMoveLearn && moveToLearn == null) {moveToLearn = moveName; showMoveLearn = true; sortState.column = moveName;}        
       }
     })
   }
@@ -76,19 +106,34 @@ function displayItems() {
     });
   }
 
-  filteredItems.forEach(item => { // Generate each list item dynamically
+  // Render the first few items
+  renderLimit = 0;
+  renderMoreItems();
+}
+
+function renderMoreItems() {
+  renderLimit += increment;
+  let slicedItems = filteredItems.slice(renderLimit-increment,renderLimit)
+  slicedItems.forEach((item, index) => { // Generate each list item dynamically
     const li = document.createElement('li'); // Entry of one Pokemon
     
     // Create each column and set its text
     const dexColumn = document.createElement('div');
     dexColumn.className = 'item-column';
+    // if (showMoveLearn) {
+    //   let dexHeader = document.querySelector('.header-container span')
+    //   dexHeader.innerHTML = 'Move'+'<br><p style="color:rgb(140, 130, 240); margin: 0;">' + (sortState.ascending ? "▲" : "▼") + '</p>';
+    //   dexColumn.innerHTML = '<b>' + item[sortState.column] + '</b>';
+    // } else {
     dexColumn.innerHTML = '<b>' + item.dexno + '</b>';
+    // }
     
     const img = document.createElement('img');
     img.src = item.img;
     img.alt = item.dexno;
     img.className = 'item-image';
-    dexColumn.appendChild(img);
+    // dexColumn.appendChild(img);
+    // dexColumn.appendChild();
 
     const specColumn = document.createElement('div');
     specColumn.className = 'item-column';
@@ -123,19 +168,25 @@ function displayItems() {
     const speColumn = document.createElement('div');
           speColumn.className = 'item-column'; speColumn.innerHTML = '<b>Spe<br>' + item.spe +'</b>';
 
-    // Append columns to the Pokemon entry
-    li.appendChild(dexColumn);      li.appendChild(img);             li.appendChild(specColumn);
-    li.appendChild(typeColumn);     li.appendChild(abilityColumn);   li.appendChild(bstColumn);
-    li.appendChild(hpColumn);       li.appendChild(atkColumn);       li.appendChild(defColumn);
-    li.appendChild(spaColumn);      li.appendChild(spdColumn);       li.appendChild(speColumn);
+    const row1 = document.createElement('div'); row1.className = 'row'; let row2 = row1;
+    
+    row1.appendChild(dexColumn);      
+    row1.appendChild(img);             
+    row1.appendChild(specColumn);
+    row1.appendChild(typeColumn);     row1.appendChild(abilityColumn);   
+    
+    if (isMobile) { row2 = document.createElement('div'); row2.className = 'row'; li.appendChild(row1); }
 
-    // Append the item to the list of Pokemon
-    itemList.appendChild(li);
+    row2.appendChild(bstColumn);
+    row2.appendChild(hpColumn);       row2.appendChild(atkColumn);       row2.appendChild(defColumn);
+    row2.appendChild(spaColumn);      row2.appendChild(spdColumn);       row2.appendChild(speColumn);    
+    
+    li.appendChild(row2); // Append the first or second row
+    itemList.appendChild(li); // Append the current entry to the list of Pokemon
   });
 }
 
-function getBackgroundClass(categ) {
-  // Return different classes based on the category
+function getBackgroundClass(categ) { // Return suggestion class based on category
   if (categ === 'Ability')    { return 'suggestion-ability'; }
   else if (categ === 'Move')  { return 'suggestion-move';    }
   else                        { return 'suggestion-default'; }
@@ -156,14 +207,15 @@ function displaySuggestions() {
           suggestionPreview = null; } 
         
         // If there is at least one locked filter, remove suggestions that have no matches
-        
+        if (lockedFilters.length > 0) {
         // Sort the list of suggestions based on hits in the item list (but still by type/ability/move)
         // (If there are no locked filters, the list is already presorted)
         
-        
+        }
         // Apply the first suggestion to the preview filter
         if (matchingSuggestions.length > 0) {suggestionPreview = matchingSuggestions[0]} 
 
+        // Remove filter preview if there are no matching suggestions
 
         suggestions.innerHTML = matchingSuggestions.map( (attr, index) => {
             let suggColor = 'rgb(255, 255, 255)' // Default color for type
@@ -195,13 +247,9 @@ function lockFilter(newLockFilter) {
     // Refresh suggestions and items
     searchBox.value = ""; // Clear the search bar after locking
     displaySuggestions();
-    displayItems();
+    refreshAllItems();
     searchBox.focus();
-    // console.log("Filter added successfully:", newLockFilter);
-  } else {
-    // console.log("Duplicate filter ignored:", newLockFilter);
   }
-  // console.log("Locked filters after adding:", lockedFilters);
 }
 
 // Remove a filter **************************
@@ -210,49 +258,90 @@ function removeFilter(filterToRemove, filterTag) {
   lockedFilters = lockedFilters.filter(
     (f) => !filterToRemove.categ.includes(f.categ) || !filterToRemove.value.includes(f.value)
   );
-  // Remove the filter tag
-  filterTag.remove();
+  filterTag.remove(); // Remove the filter tag
   // Refresh suggestions and items
   displaySuggestions();
-  displayItems();
+  refreshAllItems();
   searchBox.focus();
 }
 
 // Event listener for the header row - Clicking on the header row to sort ***************
-headerRow.addEventListener('click', event => {
-  const target = event.target.closest("span");
-  const sortAttribute = target?.getAttribute('data-sort');
+// headerContainer.addEventListener('click', event => {clickTarget(event.target.closest("span"))} );
+
+function updateHeader(clickTarget = null) {
+  console.log(clickTarget?.sortattr)
+  const sortAttribute = clickTarget?.sortattr;
   if (sortAttribute) {
     // Toggle sort direction if sorting by the same column
     if (sortState.column === sortAttribute) {
       sortState.ascending = !sortState.ascending;
     } else {
       sortState.column = sortAttribute;
-        sortState.ascending = (sortState.column == "dexno")||(sortState.column == "spec")
-                            ||(sortState.column == "type1")||(sortState.column == "ab1");
+      // Sort ascending on some columns, but descending on others
+      sortState.ascending = (sortState.column == "rowno")||(sortState.column == "spec")
+      ||(sortState.column == "type1")||(sortState.column == "ab1");
     }
-    if (currentTarget?.getAttribute('text-def')) { // Clear arrow from previous target
-      currentTarget.innerHTML = currentTarget?.getAttribute('text-def');
+    if (currentTarget?.textDef) { // Clear arrow from previous target
+      currentTarget.innerHTML = currentTarget?.textDef;
     }
-    currentTarget = target; // Draw arrow on new target
-    target.innerHTML = target?.getAttribute('text-def') + '<br><p style="color:rgb(140, 130, 240); margin: 0;">' + (sortState.ascending ? "▲" : "▼") + '</p>';
+    currentTarget = clickTarget; // Draw arrow on new target
+    clickTarget.innerHTML = clickTarget?.textDef + '<br><p style="color:rgb(140, 130, 240); margin: 0;">' + (sortState.ascending ? "▲" : "▼") + '</p>';
   }
   // Update the display
   displaySuggestions();
-  displayItems();
+  refreshAllItems();
+}
+
+function adjustLayout() {
+  const width = window.innerWidth;
+  isMobile = (width <= 768);
+  console.log((width <= 768 ? "Mobile layout" : "Desktop layout"), width, isMobile);
+  // Redraw all the header columns into the header container
+  headerContainer.innerHTML = '';
+  const thisRow = document.createElement('div'); thisRow.className = 'header-row'; let row2 = thisRow;
+  for (const thisColumn of headerColumns.slice(0,5)) {thisRow.appendChild(thisColumn);}  
+  if (isMobile) {
+    row2 = document.createElement('div'); row2.className = 'header-row'; headerContainer.appendChild(thisRow);
+  }
+  for (const thisColumn of headerColumns.slice(5,12)) {row2.appendChild(thisColumn);}  
+  headerContainer.appendChild(row2);
+}
+
+// Load more items on scroll
+window.addEventListener("scroll", () => {
+  console.log('scrolled')
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.body.scrollHeight;
+  // renderMoreItems();
+  if (window.scrollY + window.innerHeight >= document.body.scrollHeight * 0.8 - 1000) {
+    renderMoreItems();
+  }
+  console.log({
+    scrollTop: window.scrollY,
+    clientHeight: window.innerHeight,
+    scrollHeight: document.body.scrollHeight,
+  });
 });
 
 // Initial display
-displaySuggestions();
-displayItems();
+adjustLayout();
+updateHeader(headerColumns[0]);
+// displaySuggestions();
+// refreshAllItems();
 
+// Run on page load and when resizing the window
+window.addEventListener("resize", () => { 
+  adjustLayout();
+  displaySuggestions();
+  refreshAllItems();
+});
 searchBox.addEventListener('input', () => { // Typing in search box ***************
   // if (searchBox.value.length > 1) {
     displaySuggestions();
-    displayItems();
+    refreshAllItems();
   // }
 });
-
 document.addEventListener('keydown', (event) => {
   // Ignore certain keys like Tab, Shift, Control, Alt, etc.
   const ignoredKeys = ["Tab", "Shift", "Control", "Alt", "Meta", "CapsLock", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
@@ -270,7 +359,7 @@ document.addEventListener('keydown', (event) => { // Hit escape to clear the sea
     if (searchBox.value.length > 0) {
       searchBox.value = ''
       displaySuggestions();
-      displayItems();
+      refreshAllItems();
     } else if (lockedFilters.length > 0) {
       const lastFilter = lockedFilters[lockedFilters.length - 1];
       const filterTags = document.querySelectorAll(".filter-tag");
